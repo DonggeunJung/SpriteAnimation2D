27c1aa534f4dae0d9f8bba82517d5b527dd00cc6
DonggeunJung <topsan72@gmail.com>
2018-07-24 15:28:06 -0400

    Initial commit

---
+++ Assets/SealController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealController : MonoBehaviour {
+	Rigidbody2D rb2d;
+	Animator animator;
+
+	public float maxHeight;
+	public float flapVelocity;
+	public GameObject sprite;
+
+	void Awake() {
+		rb2d = GetComponent<Rigidbody2D>();
+		animator = sprite.GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Get input when unless max height
+		if (Input.GetButtonDown ("Fire1") && transform.position.y < maxHeight) {
+			Flap ();
+		}
+
+		bool bFly = false;
+		if (rb2d.velocity.y > 0)
+			bFly = true;
+		animator.SetBool("flap", bFly);
+	}
+
+	void Flap() {
+		// Accelerate to up side
+		rb2d.velocity = new Vector2(0.0f, flapVelocity);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# SpriteAnimation2D
